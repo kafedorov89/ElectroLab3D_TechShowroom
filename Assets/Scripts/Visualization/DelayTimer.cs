c8097caae425e030af83820d288e02baaf2eabee
nermakov777 <nermakov777@gmail.com>
2015-12-17 02:07:30 +0600

    All systems added

---
+++ Assets/Scripts/Visualization/DelayTimer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayTimer : MonoBehaviour{
+
+    private float startTime;
+    private float currentTime;
+    public bool Started;
+    public float Delay; //Delay in seconds
+    public float DeltaTime;
+    public bool Stoped;
+
+    public DelayTimer(float delay)
+    {
+        Delay = delay;
+        DeltaTime = 0.0f;
+    }
+
+    public DelayTimer()
+    {
+        Delay = 0.0f;
+        DeltaTime = 0.0f;
+    }
+
+    public void PrintTime()
+    {
+        //Debug.Log("DeltaTime = " + DeltaTime.ToString());
+        //Debug.Log("StartTime = " + startTime);
+        //Debug.Log("CurrentTime = " + currentTime);
+    }
+
+    public void TimerStart()
+    {
+        Started = true;
+        Stoped = false;
+        startTime = Time.fixedTime;
+        currentTime = Time.fixedTime;
+    }
+
+    public void ResetTimer()
+    {
+        TimerStop();
+        Stoped = false;
+    }
+
+    public void TimerStop()
+    {
+        Started = false;
+        Stoped = true;
+        startTime = 0.0f;
+        currentTime = 0.0f;
+    }
+
+    public bool DrinDrin()
+    {
+        currentTime = Time.fixedTime;
+        DeltaTime = Mathf.Abs(startTime - currentTime);
+
+        if (DeltaTime >= Delay)
+        {
+            TimerStop();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Update()
+    {
+        //DeltaTime = Mathf.Abs(startTime - currentTime);
+    }
+}
---
+++ Assets/Scripts/Visualization/GUIRatioScaler.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIRatioScaler : MonoBehaviour {
+
+    public RectTransform elmentRectTransform;
+    public Text textField;
+    
+    public float RelativeSizeX;
+    public float RelativeSizeY;
+    public float RelativePosX;
+    public float RelativePosY;
+    public float RelativeFontScale;
+    public int curFontSize;
+    public float curFontScale;
+
+    void Awake()
+    {
+        elmentRectTransform = GetComponent<RectTransform>();
+        
+        /*try
+        {
+            textField = GetComponent<Text>();
+        }
+        catch (MissingComponentException)
+        {
+            Debug.Log("Text element doesn't exist in root");
+            try
+            {
+                textField = GetComponentInChildren<Text>();
+            }
+            catch (MissingComponentException)
+            {
+                Debug.Log("Text element doesn't exist in children");
+            }
+        }*/
+
+        textField = GetComponent<Text>();
+        
+        if (textField == null)
+        {
+            textField = GetComponentInChildren<Text>();
+        }
+
+        //RelativeSizeX = Mathf.Abs(elmentRectTransform.rect.width / Screen.width);
+        //RelativeSizeY = Mathf.Abs(elmentRectTransform.rect.height / Screen.height);
+        //RelativePosX = Mathf.Abs(elmentRectTransform.rect.position.x / Screen.width);
+        //RelativePosY = Mathf.Abs(elmentRectTransform.rect.position.y / Screen.height);
+        RelativeSizeX = elmentRectTransform.sizeDelta.x / Screen.width;
+        RelativeSizeY = elmentRectTransform.sizeDelta.y / Screen.height;
+        RelativePosX = elmentRectTransform.localPosition.x / Screen.width;
+        RelativePosY = elmentRectTransform.localPosition.y / Screen.height;
+        
+        if (textField != null)
+        {
+            curFontSize = textField.fontSize;
+            RelativeFontScale = (float)curFontSize / Screen.height;
+        }
+    }
+
+
+    void OnGUI(){
+        elmentRectTransform.sizeDelta = new Vector2(Screen.width * RelativeSizeX, Screen.height * RelativeSizeY);
+        elmentRectTransform.localPosition = new Vector3(Screen.width * RelativePosX ,Screen.height * RelativePosY, 0);
+        
+        if (textField != null)
+        {
+            //curFontSize = curFontSize *
+            textField.fontSize = (int)Mathf.Round(Screen.height * RelativeFontScale);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
