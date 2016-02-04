d109c3e72305d8ef2e0c46be7475c613af281247
kafedorov89 <kafedorov89@gmail.com>
2016-02-04 12:17:17 +0500

    Added special effects to TCG

---
+++ Assets/Scripts/Visualization/Editor/DelayTimerEditor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+	[CustomEditor(typeof(DelayTimer))]
+	public class DelayTimerEditor : Editor {
+
+		DelayTimer delayTimer;
+		
+		public void OnEnable()
+		{
+			delayTimer = (DelayTimer)target;
+		}
+		
+		public override void OnInspectorGUI()
+		{
+			// Draw the default inspector
+			DrawDefaultInspector ();
+			
+			if (GUILayout.Button ("Timer Start")) {
+				Debug.Log ("Timer Start");
+				delayTimer.TimerStart();
+			}
+			
+			if (GUILayout.Button ("Timer Stop")) {
+				Debug.Log ("Timer Stop");
+				delayTimer.TimerStop();
+			}
+
+			if (GUILayout.Button ("Timer Reset")) {
+				Debug.Log ("Timer Reset");
+				delayTimer.ResetTimer();
+			}
+		}
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
---
+++ Assets/Scripts/Visualization/SolidTrigger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Collider))]
+public class SolidTrigger : MonoBehaviour {
+
+     //The list of colliders currently inside the trigger
+     public List<GameObject> TriggerList;
+ 
+     //called when something enters the trigger
+     void  OnTriggerEnter(Collider other)
+     {
+         //if the object is not already in the list
+         if(!TriggerList.Contains(other.gameObject))
+         {
+             //add the object to the list
+             TriggerList.Add(other.gameObject);
+         }
+     }
+ 
+     //called when something exits the trigger
+     void OnTriggerExit(Collider other)
+     {
+         //if the object is in the list
+         if (TriggerList.Contains(other.gameObject))
+         {
+             //remove it from the list
+             TriggerList.Remove(other.gameObject);
+         }
+     }
+    
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
