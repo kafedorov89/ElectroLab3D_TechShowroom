b587e61cf4021b1e6127b446b8dbb384da836cc4
Ermakov Nikita <nermakov777@gmail.com>
2016-02-06 00:15:50 +0500

    Some bugs fixed - init distance and parallel before Play Animation - load name of subsystem from file - opened generator in TCG

--- Assets/Scripts/Visualization/VisFSM.cs
+++ Assets/Scripts/Visualization/VisFSM.cs
@@ -8,7 +8,27 @@ public class VisFSM : VisClass {
 	private PlayMakerFSM unitFSM; //конечный автомат, который управляет установкой
 
 	private GameObject cameraRotation;
+	//private GameObject cameraDistance;
+	private GameObject cameraParallel;
 	private MouseOrbit orbit;
+
+	private float distance = 0.0f;
+	public float Distance
+	{
+		get 
+		{
+			if (orbit != null)
+				return orbit.Distance;
+			else
+				return 0.0f;
+		}
+		set 
+		{
+			distance = value;
+			if (orbit != null)
+				orbit.Distance = distance;
+		}
+	}
 
 	public override void StartVis()
 	{
@@ -45,6 +65,11 @@ public class VisFSM : VisClass {
 	{
 		if (currentLevel == 1) 
 		{
+			cameraParallel = GameObject.FindGameObjectWithTag ("MainCamera");
+			if (cameraParallel != null) 
+			{
+				unitFSM.FsmVariables.GetFsmGameObject ("CameraParallel").Value = cameraParallel;
+			}
 			cameraRotation = GameObject.Find ("CameraRotation");
 			if (cameraRotation != null) 
 			{
@@ -56,11 +81,8 @@ public class VisFSM : VisClass {
 		}
 	}
 
-
-
 	public float ClampAngle(float angle)
 	{
-
 		float arcsin = Mathf.Asin (Mathf.Sin (angle / 180.0f * Mathf.PI));
 		angle = arcsin * 180.0f / Mathf.PI;
 		return angle;
@@ -75,5 +97,23 @@ public class VisFSM : VisClass {
 		x = ClampAngle (x);
 		unitFSM.FsmVariables.GetFsmVector3 ("CameraRotationFrom").Value = new Vector3 (x, y, z);
 	}
+
+	/*public void SetDistanceFrom()
+	{
+		if (orbit != null)
+			unitFSM.FsmVariables.GetFsmFloat ("DistanceFrom").Value = - orbit.cameraDistance.transform.localPosition.z;
+	}
+
+	public void SetDistance()
+	{
+		if (orbit != null)
+			orbit.Distance = unitFSM.FsmVariables.GetFsmFloat ("Distance").Value;
+	}
+
+	public void ResetCameraParallel()
+	{
+		if (orbit != null)
+			orbit.transform.localPosition = new Vector3 (0,0,0);
+	}*/
 }
 
